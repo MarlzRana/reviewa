cd3d748e5a3ae21e9d22d2ebbb7010a784b6f3f5
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2026-03-28 13:16:29 +0000

    feat: codex support

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,14 @@ import * as vscode from 'vscode';
 import { CommentStore } from './commentStore';
 import { createReviewaCommentController } from './commentController';
 import { createFileWatcher } from './fileWatcher';
-import { installHookScript, registerHook } from './hookManager';
+import { installHookScript, registerHooks } from './hookManager';
 
 let store: CommentStore;
 
 export function activate(context: vscode.ExtensionContext) {
 	CommentStore.ensureDirectoryExists();
 	installHookScript();
-	registerHook();
+	registerHooks();
 
 	store = new CommentStore();
 	createReviewaCommentController(context, store);
